f24acec56dc4a432cc31c492c238af0b626ef797
RGPin <157803400+RGPin@users.noreply.github.com>
2026-05-13 15:21:03 +0800

    Public static files, add new route to view details

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -23,9 +23,20 @@ indexRouter.get("/new", (req, res) => {
   res.render("form");
 });
 
+indexRouter.get("/details/:user", (req, res) => {
+  const { user } = req.params;
+  const messageObj = messages.find((message) => user === message.user);
+  if (!messageObj) res.send("Message not found");
+  res.render("details", { messageObj });
+});
+
 indexRouter.post("/new", (req, res) => {
   const { author, message } = req.body;
-  messages.push({ text: message, user: author, added: new Date() });
+  messages.push({
+    text: message,
+    user: author,
+    added: new Date(),
+  });
   res.redirect("/");
 });
 
