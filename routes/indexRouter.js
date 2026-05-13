57cb8d2b63730eb947b5ad2c7da72fe2a3372c17
RGPin <157803400+RGPin@users.noreply.github.com>
2026-05-13 14:43:00 +0800

    index ejs with indexRouter

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -2,6 +2,21 @@ const express = require("express");
 
 const indexRouter = express.Router();
 
-indexRouter.get("/", (req, res) => res.send("Hello my guy"));
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date(),
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date(),
+  },
+];
+
+indexRouter.get("/", (req, res) => {
+  res.render("index", { title: "Mini Messageboard", messages });
+});
 
 module.exports = indexRouter;
