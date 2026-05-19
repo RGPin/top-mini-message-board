31abcb08c9e70b156c81d46e2d01a1618023ee0a
RGPin <157803400+RGPin@users.noreply.github.com>
2026-05-19 15:08:58 +0800

    Add validation

--- controllers/indexControllers.js
+++ controllers/indexControllers.js
@@ -1,4 +1,22 @@
 const db = require("../db/queries");
+const { body, validationResult, matchedData } = require("express-validator");
+
+const lengthErr = "must be between 1 and 10 characters.";
+
+const validateUser = [
+  body("username")
+    .trim()
+    .notEmpty()
+    .withMessage("Enter username")
+    .isLength({ max: 10 })
+    .withMessage(`Username ${lengthErr}`),
+  body("text")
+    .trim()
+    .notEmpty()
+    .withMessage("Enter message")
+    .isLength({ max: 500 })
+    .withMessage("Message must be 500 characters or below."),
+];
 
 async function getMessages(req, res) {
   const messages = await db.getMessages();
@@ -12,11 +30,18 @@ async function addMessageGet(req, res) {
   res.render("form");
 }
 
-async function addMessagePost(req, res) {
-  const { text, username } = req.body;
-  await db.addMessage({ text, username });
-  res.redirect("/");
-}
+const addMessagePost = [
+  ...validateUser,
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).render("form", { errors: errors.array() });
+    }
+    const { text, username } = matchedData(req);
+    await db.addMessage({ text, username });
+    res.redirect("/");
+  },
+];
 
 async function getMessageDetails(req, res) {
   const { id } = req.params;
