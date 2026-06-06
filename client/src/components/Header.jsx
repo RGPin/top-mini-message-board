20e7298c45993b4eee34bbb3aa77f16d2e465c92
RGPin <157803400+RGPin@users.noreply.github.com>
2026-06-06 13:43:02 +0800

    home styling on mobile

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -2,24 +2,40 @@ import { Link, Route, Routes } from "react-router";
 import App from "../App";
 import AddMessage from "../pages/AddMessage";
 import Home from "../pages/Home";
+import HomeIcon from "./icons/HomeIcon";
+import AddMessageIcon from "./icons/AddMessageIcon";
+import MessageIcon from "./icons/MessageIcon";
 
 export default function Header() {
   return (
     <header className="main-header">
       <div className="header-logo">
-        <Link to="/">✉️ Mini Messageboard</Link>
+        <Link to="/">
+          <h1 className="header-title">
+            <span className="header-title-icon">
+              <MessageIcon />
+            </span>{" "}
+            <span className="header-title-text">Mini Messageboard</span>
+          </h1>
+        </Link>
       </div>
 
       <nav className="header-nav">
         <ul className="nav-list">
           <li className="nav-item">
             <Link to="/" className="nav-link">
-              Home
+              <span className="link-icon">
+                <HomeIcon />
+              </span>
+              <span className="link-text">Home</span>
             </Link>
           </li>
           <li className="nav-item">
             <Link to="/add-message" className="nav-link">
-              Add Message
+              <span className="link-icon">
+                <AddMessageIcon />
+              </span>
+              <span className="link-text">Add Message</span>
             </Link>
           </li>
         </ul>
